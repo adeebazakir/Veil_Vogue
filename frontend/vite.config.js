a211af7318b719d27117c6a9318d7bc83f659985
Adeeba <shafaqshanza@gmail.com>
2025-10-29 02:56:49 +0530

    changed vite route to versel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,28 @@
+// // vite.config.js
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react'; // Import the React plugin
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   // The plugins array tells Vite to use the React plugin
+//   plugins: [react()], 
+// });
+
+
 // vite.config.js
-
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Import the React plugin
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // The plugins array tells Vite to use the React plugin
-  plugins: [react()], 
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL, // your backend deployed on Render
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
